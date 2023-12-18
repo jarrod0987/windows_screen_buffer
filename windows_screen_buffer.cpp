6dba10b3c7848efc2d44b47b89561a383f0ad398
Jarrod0987 <jarrod0987@yahoo.com>
2023-12-18 16:57:45 -0500

    ..

--- windows_screen_buffer.cpp
+++ windows_screen_buffer.cpp
@@ -1,34 +1,32 @@
 #include<iostream>
+#include<memory>
 #include<Windows.h>
 
 using namespace std;
 
 int main()
 {
-	int screen_width = 120;
+	int screen_width = 80;
 	int screen_height = 30;
 	int total = screen_width * screen_height;
-
-	wchar_t* screen = new wchar_t[total]{};//Don't forget to initialize.
-			
-	//Display a * at end of buffer.
-	//screen[((screen_width-1) * screen_height) + screen_height-1] = L' ';
-
+	
+	shared_ptr<wchar_t[]> screen(new wchar_t[total], default_delete<wchar_t[]>());
+	fill(screen.get(), screen.get()+total, L' ');
+	
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
-	DWORD dwBytesWritten = 0;
+	DWORD bytes_written = 0;
 
 	while (true)
 	{
 		//Draw Stats & Border.
 		for (int i{}; i < screen_width; i++)
 		{
-			screen[i] = L'_';
-			screen[screen_width * 2 + i] = L'_';
+			screen.get()[i] = L'_';
+			screen.get()[screen_width * 2 + i] = L'_';
 		}
 
 		//Display Frame.
-		WriteConsoleOutputCharacter(hConsole, screen, total, {0,0}, &dwBytesWritten);
+		WriteConsoleOutputCharacter(hConsole, screen.get(), total, {0,0}, &bytes_written);
 	}
-	//There should be delete[] screen here but it never gets called due to infinite while loop.
 }
