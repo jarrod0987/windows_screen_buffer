a825b1d81a165538fa75768e3de7ca1d181c64d8
Jarrod0987 <jarrod0987@yahoo.com>
2023-12-18 17:04:12 -0500

    words

--- windows_screen_buffer.cpp
+++ windows_screen_buffer.cpp
@@ -9,10 +9,9 @@ int main()
 	int screen_width = 80;
 	int screen_height = 30;
 	int total = screen_width * screen_height;
-	
-	shared_ptr<wchar_t[]> screen(new wchar_t[total], default_delete<wchar_t[]>());
-	fill(screen.get(), screen.get()+total, L' ');
-	
+
+	shared_ptr<wchar_t[]> screen(new wchar_t[total] {}, default_delete<wchar_t[]>());
+		
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD bytes_written = 0;
